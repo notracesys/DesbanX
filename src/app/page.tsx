5aac45c8987c165a0285898f8736fc51b1fad97c
Stullet <lirabusiness7@gmail.com>
2026-01-07 16:39:58 +0000

    o video esta um pouquinho abaixo, deixe bem certinho, bem embaixo do hea

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,10 +8,10 @@ export default function Home() {
   return (
     <div className="flex min-h-screen flex-col">
       <Header />
-      <main className="relative flex flex-col items-center justify-center min-h-[calc(100vh-5rem)]">
+      <main className="relative flex flex-col min-h-[calc(100vh-5rem)]">
         <BackgroundVideo />
         <div className="absolute inset-0 bg-black/50" />
-        <div className="relative z-10 container mx-auto px-4 py-8 md:py-16">
+        <div className="relative z-10 flex-grow flex flex-col items-center justify-center container mx-auto px-4 py-8 md:py-16">
           <Landing />
         </div>
       </main>
