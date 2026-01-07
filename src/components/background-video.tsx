505e6f8975b12575707545a9e7264d5dcf5cb876
Stullet <lirabusiness7@gmail.com>
2026-01-07 16:33:01 +0000

    é .mp4

--- src/components/background-video.tsx
+++ src/components/background-video.tsx
@@ -7,7 +7,7 @@ export default function BackgroundVideo() {
       playsInline
       className="fixed top-0 left-0 w-full h-full object-cover -z-10"
     >
-      <source src="/sasuke.3840x2160" />
+      <source src="/sasuke.3840x2160.mp4" type="video/mp4" />
       Your browser does not support the video tag.
     </video>
   );
