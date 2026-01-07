9ed4c19e423b29a683c257d62c3c87f53adef22d
Stullet <lirabusiness7@gmail.com>
2026-01-07 16:31:39 +0000

    pronto. o nome é sasuke.3840x2160

--- src/components/background-video.tsx
+++ src/components/background-video.tsx
@@ -7,7 +7,7 @@ export default function BackgroundVideo() {
       playsInline
       className="fixed top-0 left-0 w-full h-full object-cover -z-10"
     >
-      <source src="https://assets.mixkit.co/videos/preview/mixkit-futuristic-plexus-network-connections-in-motion-32791-large.mp4" type="video/mp4" />
+      <source src="/sasuke.mp4" type="video/mp4" />
       Your browser does not support the video tag.
     </video>
   );
