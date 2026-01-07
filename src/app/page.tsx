691c2d3406282fe849ef8dc18092059b880c1000
Stullet <lirabusiness7@gmail.com>
2026-01-07 16:36:56 +0000

    ainda não esta aparecendo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen flex-col bg-black">
       <Header />
-      <main className="relative flex-grow flex flex-col items-center justify-center">
+      <main className="relative flex flex-col items-center justify-center min-h-[calc(100vh-5rem)]">
         <BackgroundVideo />
         <div className="absolute inset-0 bg-black/50" />
         <div className="relative z-10 container mx-auto px-4 py-8 md:py-16">
