695aa1da3b511f0a9a69fb6e4d552d472e8688c2
Stullet <lirabusiness7@gmail.com>
2026-01-07 16:42:47 +0000

    não é para aumentar o tamanho de proporçao do video, coloque um gradient

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,8 +10,8 @@ export default function Home() {
       <Header />
       <main className="relative flex flex-col min-h-[calc(100vh-5rem)]">
         <BackgroundVideo />
-        <div className="absolute inset-0 bg-black/50" />
-        <div className="relative z-10 flex-grow flex flex-col items-center justify-center container mx-auto px-4 py-8 md:py-16 pt-64">
+        <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black/80" />
+        <div className="relative z-10 flex flex-grow flex-col items-center justify-center container mx-auto px-4 py-8 md:py-16">
           <Landing />
         </div>
       </main>
