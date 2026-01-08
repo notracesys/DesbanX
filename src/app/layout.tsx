2210660713aae2964fbf383dfacbc696c91ade6c
Stullet <lirabusiness7@gmail.com>
2026-01-08 00:34:07 +0000

    O TITULO DEVE SER DEXBAN FF (NO HEAD)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 
 export const metadata: Metadata = {
-  title: 'Recupere sua conta de free fire',
+  title: 'DEXBAN FF',
   description: 'Análise e auxílio para recuperação de contas do Free Fire banidas injustamente.',
 };
 
