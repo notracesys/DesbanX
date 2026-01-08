e98ca4872994c0f3826a10ef775db903a357fdd3
Stullet <lirabusiness7@gmail.com>
2026-01-08 00:05:54 +0000

    voce não tirou o aviso legal do chat]

---
+++ src/app/chat/layout.tsx
@@ -0,0 +1,14 @@
+import { Toaster } from '@/components/ui/toaster';
+
+export default function ChatLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      {children}
+      <Toaster />
+    </>
+  );
+}
