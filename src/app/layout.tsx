2f87ae54a73fa3b1e2c31fc4f045997c4f5f53cf
Stullet <lirabusiness7@gmail.com>
2026-01-08 00:53:41 +0000

    ainda não esta aparecendo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="pt-BR" className="dark">
       <head>
+        <link rel="icon" href="/favicon.png?v=1" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
