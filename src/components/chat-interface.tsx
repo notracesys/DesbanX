0d7aae7c0d41173cc1dcc1f2751305b69bc95746
Stullet <lirabusiness7@gmail.com>
2026-01-15 15:13:11 +0000

    não quero aqui nessas paginas. quero essa logo somente na foto de perfil

--- src/components/chat-interface.tsx
+++ src/components/chat-interface.tsx
@@ -437,3 +437,5 @@ Descrição do ocorrido:
     </>
   );
 }
+
+    
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,7 +6,7 @@ import { useState, useEffect } from 'react';
 
 const Logo = () => (
     <Link href="/" className="flex items-center gap-4">
-        <Image src="/logo2.png" alt="DEXBAN FF Logo" width={120} height={34} />
+        <Image src="/logo.png" alt="DEXBAN FF Logo" width={120} height={34} />
     </Link>
 );
 
@@ -75,3 +75,5 @@ export default function Header() {
     </header>
   );
 }
+
+    
