699ffd7315a5a989a669c4c303b4990401f91445
Stullet <lirabusiness7@gmail.com>
2026-01-27 01:53:03 +0000

    pode subir o conteudo la pra cima denovo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,12 +10,12 @@ export default function Home() {
     <div className="flex min-h-full flex-col">
       <BrowserCheckDialog />
       <Header />
-      <main className="flex-grow flex flex-col -mt-4">
-        <div className="relative h-[70vh] w-full bg-background">
+      <main className="flex-grow flex flex-col -mt-16">
+        <div className="relative h-screen w-full bg-background">
           <ParticleBackground />
           <div className="absolute inset-0 bg-gradient-to-b from-transparent to-background" />
         </div>
-        <div className="py-8">
+        <div className="absolute inset-0 flex items-center justify-center">
           <div className="relative z-10 container mx-auto px-4">
             <Landing />
           </div>
