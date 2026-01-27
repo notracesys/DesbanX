5a9ff2da3805e26d5236ac30817711157545e41c
Stullet <lirabusiness7@gmail.com>
2026-01-27 17:42:02 +0000

    não quero assim, apague essa alteraçao

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,11 +12,8 @@ export default function Home() {
       <Header />
       <main className="flex-grow">
         <ParticleBackground />
-        <div className="fixed inset-0 bg-gradient-to-t from-background via-background/80 to-transparent" />
-        <div className="relative z-10 flex h-full min-h-[calc(100vh-4rem)] items-end justify-center pb-12">
-          <div className="container mx-auto px-4">
-            <Landing />
-          </div>
+        <div className="container mx-auto px-4 flex h-full min-h-[calc(100vh-4rem)] items-end justify-center pb-12">
+          <Landing />
         </div>
       </main>
     </div>
