af0358e5c3d97f883aff88cde5d37bc2e1bfc489
Stullet <lirabusiness7@gmail.com>
2026-01-27 01:45:41 +0000

    salvei como videofundo.mp4

--- src/components/background-video.tsx
+++ src/components/background-video.tsx
@@ -7,7 +7,7 @@ export default function BackgroundVideo() {
       playsInline
       className="absolute top-0 left-0 w-full h-full object-cover"
     >
-      <source src="/sasuke.3840x2160.mp4" type="video/mp4" />
+      <source src="/videofundo.mp4" type="video/mp4" />
       Your browser does not support the video tag.
     </video>
   );
