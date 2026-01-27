5db18a0476c75cb3e43e7576a0196ac858c84fc0
Stullet <lirabusiness7@gmail.com>
2026-01-27 16:48:04 +0000

    adicione o video de fundo denovo e desça o conteudo la pra baixo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       <BrowserCheckDialog />
       <Header />
       <main className="flex-grow">
-        <div className="relative flex h-full min-h-[calc(100vh-4rem)] items-center justify-center">
+        <div className="relative flex h-full min-h-[calc(100vh-4rem)] items-end justify-center pb-24">
           <ParticleBackground />
           <div className="absolute inset-0 bg-gradient-to-t from-background via-background/80 to-transparent" />
           <div className="relative z-10 container mx-auto px-4">
