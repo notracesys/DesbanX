e91c5cc333b9eb53a225c1fd521cc2846f3fe73a
Stullet <lirabusiness7@gmail.com>
2026-01-27 16:51:34 +0000

    desça todo o conteudo para abaixo da metade do video

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       <BrowserCheckDialog />
       <Header />
       <main className="flex-grow">
-        <div className="relative flex h-full min-h-[calc(100vh-4rem)] items-end justify-center pb-24">
+        <div className="relative flex h-full min-h-[calc(100vh-4rem)] items-end justify-center pb-12">
           <ParticleBackground />
           <div className="absolute inset-0 bg-gradient-to-t from-background via-background/80 to-transparent" />
           <div className="relative z-10 container mx-auto px-4">
