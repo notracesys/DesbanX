8f76d6bd2cd6f2bf0c07a64d3dd482cfb2cf060b
Stullet <lirabusiness7@gmail.com>
2026-01-28 02:37:48 +0000

    adicione o gradiente no video, deixe o bottom dele com uma transiçao par

--- src/components/particle-background.tsx
+++ src/components/particle-background.tsx
@@ -18,13 +18,13 @@ export default function ParticleBackground({
           muted
           playsInline
           className={cn(
-            'absolute left-0 h-screen w-full object-cover top-[130px] md:top-[-120px]',
+            'absolute left-0 h-screen w-full object-cover top-[-30px] md:top-[-30px]',
             className
           )}
         >
           <source src="/videofundo.mp4" type="video/mp4" />
         </video>
-        <div className="absolute bottom-0 left-0 right-0 h-1/3 bg-gradient-to-t from-background to-transparent z-10" />
+        <div className="absolute bottom-0 left-0 right-0 h-2/5 bg-gradient-to-t from-black to-transparent z-10" />
     </div>
   );
 }
