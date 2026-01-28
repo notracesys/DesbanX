6bd1108f5e13acfada671f8b834062e7fdf475d5
Stullet <lirabusiness7@gmail.com>
2026-01-28 02:30:40 +0000

    agora coloque um gradiente no bottom do video com a cor do fundo.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
     <div className="relative min-h-full">
       <BrowserCheckDialog />
       <ParticleBackground />
-      <div className="absolute inset-0 bg-gradient-to-t from-background via-background/30 to-transparent" />
+      <div className="absolute bottom-0 left-0 right-0 h-1/3 bg-gradient-to-t from-background to-transparent" />
       <div className="relative z-10 flex min-h-full flex-col">
         <Header />
         <main className="flex-grow">
