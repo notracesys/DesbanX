b3ef05c3b5cbc9f5c56a97af75efb2b4d15bfc20
Stullet <lirabusiness7@gmail.com>
2026-01-28 16:18:34 +0000

    troque a imagem da equipe desbanx pela imagem na pasta public chamada eq

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,7 +7,7 @@ import { useState, useEffect } from 'react';
 const Logo = () => (
     <Link href="/" className="flex items-center gap-1">
         <Avatar className="h-9 w-9">
-            <AvatarImage src="/logo2.jpg" alt="@desbanx profile picture" />
+            <AvatarImage src="/equipedebanx.jpg" alt="Equipe Desbanx profile picture" />
             <AvatarFallback>DX</AvatarFallback>
         </Avatar>
         <span className="font-semibold text-lg text-foreground">@desbanx</span>
