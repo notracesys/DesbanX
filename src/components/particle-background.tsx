1e619d3c1191cdcc3ac11c3cad82068d821eb240
Stullet <lirabusiness7@gmail.com>
2026-01-28 02:34:04 +0000

    não esta aparecendo o gradiente

--- src/components/particle-background.tsx
+++ src/components/particle-background.tsx
@@ -18,13 +18,13 @@ export default function ParticleBackground({
           muted
           playsInline
           className={cn(
-            'absolute left-0 h-screen w-full object-cover top-[-30px] md:top-[-80px]',
+            'absolute left-0 h-screen w-full object-cover top-[-330px] md:top-[-280px]',
             className
           )}
         >
           <source src="/videofundo.mp4" type="video/mp4" />
         </video>
-        <div className="absolute bottom-0 left-0 right-0 h-1/3 bg-gradient-to-t from-background to-transparent" />
+        <div className="absolute bottom-0 left-0 right-0 h-1/3 bg-gradient-to-t from-background to-transparent z-10" />
     </div>
   );
 }
