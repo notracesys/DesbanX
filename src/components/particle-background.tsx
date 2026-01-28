4b04eeb2c152a0990675f795c39cf38f3bc6fbd5
Stullet <lirabusiness7@gmail.com>
2026-01-28 02:02:59 +0000

    desloque mais para cima

--- src/components/particle-background.tsx
+++ src/components/particle-background.tsx
@@ -17,7 +17,7 @@ export default function ParticleBackground({
       muted
       playsInline
       className={cn(
-        'absolute left-0 -top-48 w-full h-full object-cover',
+        'absolute left-0 -top-96 w-full h-full object-cover',
         className
       )}
     >
